3cfd72022183d35711c490086d91b042ffd044fb
kongzhilv <157718507+kongzhilv@users.noreply.github.com>
2026-08-10 17:03:37 +0800

    fix(build): restore macOS tray speed module

--- src-tauri/src/utils/mod.rs
+++ src-tauri/src/utils/mod.rs
@@ -14,6 +14,8 @@ pub mod server;
 pub mod singleton;
 pub mod speed;
 pub mod tmpl;
+#[cfg(target_os = "macos")]
+pub mod tray_speed;
 pub mod window_manager;
 #[cfg(target_os = "windows")]
 pub mod windows_network;
